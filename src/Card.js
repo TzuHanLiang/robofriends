ee2810cc263662481c2f3349feb568225b6dd62c
crocodilestear <crocodilestear@gmail.com>
2018-09-16 20:55:17 +0800

    commit my robofriends project

---
+++ src/Card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Card = ({id, name, email}) => {
+	return(
+		<div className='bg-light-pink dib pa3 br3 ma2 grow bw2 shadow-5'>
+			<img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+			<div>
+			<h2>{name}}</h2>
+			<p>{email}}</p>
+			</div>
+		</div>
+	);
+}
+// const Card = (prop) => {
+// 	const {id, name, email} = prop;
+// 	return(
+// 		<div className='bg-light-pink dib pa3 br3 ma2 grow bw2 shadow-5'>
+// 			<img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+// 			<div>
+// 			<h2>{name}}</h2>
+// 			<p>{email}}</p>
+// 			</div>
+// 		</div>
+// 	);
+// }
+// const Card = (prop) => {
+// 	return(
+// 		<div className='bg-light-pink dib pa3 br3 ma2 grow bw2 shadow-5'>
+// 			<img alt='robot' src={`https://robohash.org/${prop.id}?200x200`}/>
+// 			<div>
+// 			<h2>{prop.name}</h2>
+// 			<p>{prop.email}</p>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+export default Card
