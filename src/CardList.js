ffb0d450660bf964a4494c9701f4dedf7c37187a
crocodilestear <crocodilestear@gmail.com>
2018-09-16 21:06:20 +0800

    destructure syntax: wrap javascript sytax inside the JSX

--- src/CardList.js
+++ src/CardList.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-	const CardComponet = robots.map((user, i) => {
-		return (
-			<Card 
-			key={robots[i].id} 
-			id={robots[i].id} 
-			name={robots[i].name} 
-			email={robots[i].email}
-			/>
-		);		
-	})
 
 	return (
 		<div>
-	 		{CardComponet}
+	 		{
+	 			robots.map((user, i) => {
+					return (
+						<Card 
+						key={robots[i].id} 
+						id={robots[i].id} 
+						name={robots[i].name} 
+						email={robots[i].email}
+						/>
+					);		
+				})
+
+	 		}
 		</div>
 	);
 }
